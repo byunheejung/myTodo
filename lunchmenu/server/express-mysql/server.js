ebee085400a6145708b77533773129bcd5df205c
변희정 <byunheejung@byeonhuijeong-ui-iMac.local>
2019-11-26 11:37:33 +0900

    폴더 셋팅 구조 변경 및 피요없는 폴더 삭제

--- lunchmenu/server/express-mysql/server.js
+++ lunchmenu/server/express-mysql/server.js
@@ -6,25 +6,24 @@ var connection = mysql.createConnection(dbconfig);
 
 var app = express();
 
+app.set('views', __dirname + '/public')
 // configuration
 app.set('port', process.env.PORT || 3000);
-
+app.use(express.static('../../public'))
 app.get('/', function(req, res) {
-  res.sendFile('menu/index.html', {
-    root: __dirname + '../../../'
-  });
+  res.render('index.html');
 });
 
-app.get('/', function(req, res) {
-  // connection.query('SELECT * from Persons', function(err, rows) {
-  //   if (err) {
-  //     throw err;
-  //   }
+// app.get('/', function(req, res) {
+//   // connection.query('SELECT * from Persons', function(err, rows) {
+//   //   if (err) {
+//   //     throw err;
+//   //   }
 
-  //   console.log('The SOlution is : ', rows);
-  //   res.send(rows);
-  // });
-});
+//   //   console.log('The SOlution is : ', rows);
+//   //   res.send(rows);
+//   // });
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Express server listeneing on port ' + app.get('port'));
