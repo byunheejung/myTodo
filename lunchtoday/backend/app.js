d97110d50052e10d2f5d17f0eddcf41ab0e335d8
anjelra <puppy912000@naver.com>
2019-12-10 17:58:53 +0900

    express랑 vue 연동까지는 끝

--- lunchtoday/backend/app.js
+++ lunchtoday/backend/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
 
+const express = require('express');
 const app = express();
 
 let port = 3000;
@@ -7,4 +7,14 @@ let port = 3000;
 app.listen(port, () => {
     // eslint-disable-next-line no-console
     console.log('서버가 ${port}에서 동작중입니다.');
-});
+});  
+
+const indexRouter = require('./routes/index');
+
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+
+const history = require('connect-history-api-fallback');
+
+app.use(history());
---
+++ lunchtoday/vue.config.js
@@ -0,0 +1,13 @@
+var path = require('path');
+
+module.exports = {
+    outputDir: path.resolve(__dirname, 'backend/public/'),
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true
+            }
+        }
+    }
+}
