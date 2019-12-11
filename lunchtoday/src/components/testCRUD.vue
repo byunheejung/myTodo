f242e27700c691924634fd0c52341dde759a38f9
anjelra <puppy912000@naver.com>
2019-12-11 16:13:19 +0900

    test  페이지 만들기

---
+++ lunchtoday/src/components/testCRUD.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <button class="btn-test-add">메뉴추가</button>
+        <button class="btn-test-delete">메뉴삭제</button>
+        <button class="btn-test-modify">메뉴수정</button>
+        <button class="btn-test-search">메뉴조회</button>
+    </section>    
+</template>
+
+<script>
+export default {
+  name: 'testCRUD',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
