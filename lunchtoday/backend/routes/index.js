cba266477c70b89180243416672b5c395cdc89fb
anjelra <puppy912000@naver.com>
2019-12-12 18:19:11 +0900

    다시 초심을 가지고

--- lunchtoday/backend/routes/index.js
+++ lunchtoday/backend/routes/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const addGroup = require('./addGroup');
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
-
-router.use(addGroup);
 
 module.exports = router;
