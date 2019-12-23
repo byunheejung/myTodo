036c00b4fd92f3667047d7c9d02c2d278fd2b491
anjelra <puppy912000@naver.com>
2019-12-23 11:27:40 +0900

    cros(cross origin resource sharing) 이슈 일단 해보기

--- lunchtoday/backend/routes/index.js
+++ lunchtoday/backend/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
+    res.set({'access-control-allow-origin': '*'});
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
     // res.sendFile(path.join(__dirname, '../src', 'main.js'));
 });
